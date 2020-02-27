33944d2a6f352c204bc3e41de418514d949dbdb5
Nick Lubyanov <lubyanov@gmail.com>
2020-02-27 20:18:40 +0300

    main file fix

--- app.py
+++ app.py
@@ -15,18 +15,26 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
 
-    if HAR_TO_OAS_FILE_PATH:
-        data_to_parse, read_success = HarReader().read_from_file(HAR_TO_OAS_FILE_PATH)
-        if read_success:
-            har_parser = HarParser(data_to_parse)
-            api_calls = har_parser.parse()
-            oas_processor = OasProcessor(api_calls)
-            oas_data = oas_processor.process_dict()
-            if OasWriter(oas_data).write():
-                logger.info("Converting successfully done")
-            else:
-                logger.error(f"Can't write result of convert")
-        else:
-            logger.error(f"Can't read data from {HAR_TO_OAS_FILE_PATH!r}")
-    else:
+    if not HAR_TO_OAS_FILE_PATH:
         logger.error(f"Variable 'HAR_TO_OAS_FILE_PATH' must be set")
+        exit(1)
+
+    data_to_parse, read_success = HarReader().read_from_file(HAR_TO_OAS_FILE_PATH)
+
+    if not read_success:
+        logger.error(f"Can't read data from {HAR_TO_OAS_FILE_PATH!r}")
+        exit(1)
+
+    har_parser = HarParser(data_to_parse)
+    api_calls = har_parser.parse()
+    oas_processor = OasProcessor(api_calls)
+    oas_data = oas_processor.process_dict()
+
+    write_success = OasWriter(oas_data).write()
+
+    if not write_success:
+        logger.error(f"Can't write result of convert")
+        exit(1)
+
+    logger.info("Converting successfully done")
+    exit(0)
